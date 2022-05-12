15b143be200021e70193866914d4c0da1f2ae7c3
Taha Khawaja <67346760+tahakhawaja@users.noreply.github.com>
2022-05-12 03:25:05 -0300

    Delete config.py

--- container_content/config.py
+++
@@ -1,8 +0,0 @@
-# Repository Config file
-## Containing all credentials & tokens
-
-GITHUB_API_KEY = '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'
-SONARCLOUD_API_KEY = '748397f4d2597e4aca48ed6d22469bf8140da485'
-
-GITHUB_USERNAME = 'tahakhawaja'
-GITHUB_PASSWORD = 'szirChun80'
